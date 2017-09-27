b6deb1721ac92fd17d7363cd95d6d23a3c9e0f79
Casey Handmer <casey.handmer@gmail.com>
2017-09-26 22:54:40 -0700

    added formspree form url

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "http://formspree.io/feral.robotics@gmail.com",//"././mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
